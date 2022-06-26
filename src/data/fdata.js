f7ac5a9b47176e3fbb8e7634a1aaee4631deba98
atanu <atanuj383@gmail.com>
2022-06-26 21:11:37 +0530

    deploy

--- src/data/fdata.js
+++ src/data/fdata.js
@@ -1,4 +1,4 @@
-export const apilink = 'http://localhost:5000';
+export const apilink = 'https://remindm.herokuapp.com/';
 // // export const socketlink = 'http://localhost:9000';
 // export const socketlink = 'https://moon-socket.herokuapp.com';
 // export const apilink = 'https://moonn-jobs.herokuapp.com';
